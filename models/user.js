619d228c6b20618e9bcef5351e730f066fe7843e
Preeti Duraipandian <preetidp90@gmail.com>
2018-04-20 03:09:15 +0000

    Added admin functionality

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ var passportLocalMongoose=require("passport-local-mongoose");
 var userSchema=new mongoose.Schema({
     username:String,
     password:String,
-    email:String
+    email:String,
+    isAdmin:Boolean
 });
 userSchema.plugin(passportLocalMongoose);
 module.exports=mongoose.model("User",userSchema);
