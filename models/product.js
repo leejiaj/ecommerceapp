7a718761320807c45ddc439f14f4e7141ad20de2
Leejia James <leejiaj@gmail.com>
2018-04-24 17:39:53 -0500

    leejias product changes

--- models/product.js
+++ models/product.js
@@ -1,10 +1,20 @@
 var mongoose=require("mongoose");
-    
-var productSchema={
-    name:String,
-    price:String,
-    image:String,
-    description:String,
-};
+var passportLocalMongoose=require("passport-local-mongoose");
 
+var productSchema=new mongoose.Schema({
+    productid:String,
+    productname:String,
+    description:String,
+    productimage1:String,
+    productimage2:String,
+    productimage3:String,
+    productimage4:String,
+    productimage5:String,
+    vendorid:String,
+    stockavailable:String,
+    rating:String,
+    price:String,
+    categoryid:String
+});
+productSchema.plugin(passportLocalMongoose);
 module.exports=mongoose.model("Product",productSchema);
